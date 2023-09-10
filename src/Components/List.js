84e73898a2a10ae4c930e04a1f415aff99aedb08
olegior <10z@tut.by>
2023-09-10 17:24:23 +0300

    fix list

--- src/Components/List.js
+++ src/Components/List.js
@@ -6,7 +6,7 @@ export const List = ({ data = [], cb }) => {
         <div className='component'>
             <ul>
                 {data.map((e, i) => <li key={e + i} className='component row' data-value={e}>
-                    <p className='break-words'>{e}</p>
+                    <p className='break-all'>{e}</p>
                     <Button title={'!!!'} cb={cb} datavalue={i}/>
                 </li>)}
             </ul>
